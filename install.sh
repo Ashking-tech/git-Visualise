c60f23485a53354e7ee3ccb3e92ee785a5ca4a67
Ashking <Ashking21@proton.me>
2026-02-28 11:41:19 +0530

    Fix install script

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+set -e
 
 BINARY_NAME="git-visualize"
 REPO_URL="https://github.com/Ashking-tech/git-Visualise"
 
 echo "Installing ${BINARY_NAME}..."
 
-# Direct download URL from release
-curl -sSL "${REPO_URL}/releases/download/v1.0.0/${BINARY_NAME}" -o "/tmp/${BINARY_NAME}"
-
-# Move to system path
+curl -L "${REPO_URL}/releases/download/v1.0.0/${BINARY_NAME}" -o "/tmp/${BINARY_NAME}"
+chmod +x "/tmp/${BINARY_NAME}"
 sudo mv "/tmp/${BINARY_NAME}" "/usr/local/bin/${BINARY_NAME}"
-sudo chmod +x "/usr/local/bin/${BINARY_NAME}"
 
 echo "Installed! Run: git-visualize -email your@email.com"
